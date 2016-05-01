90c1248a8a141f9bc684726a7f2f1260b36be34a
zegelink <supercomboooo@gmail.com>
2016-04-30 18:38:11 -0700

    Basic layout for login and register, add a link in main to Login

--- app/src/main/java/zheng/studybuddy/RegisterActivity.java
+++ app/src/main/java/zheng/studybuddy/RegisterActivity.java
@@ -1,7 +1,12 @@
 package zheng.studybuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
     }
 }
