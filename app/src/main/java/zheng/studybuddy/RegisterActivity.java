441f007f779fbe455e2573d74c0592a4ffe2397e
zegelink <supercomboooo@gmail.com>
2016-04-30 23:10:35 -0700

    Set up the register, it is working now
    Add internet permission in the manifest, set up the server to store
    username and password

--- app/src/main/java/zheng/studybuddy/RegisterActivity.java
+++ app/src/main/java/zheng/studybuddy/RegisterActivity.java
@@ -1,12 +1,20 @@
 package zheng.studybuddy;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -20,5 +28,39 @@ public class RegisterActivity extends AppCompatActivity {
 
         final Button bRegister = (Button) findViewById(R.id.bRegister);
 
+        bRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final String email = etEmail.getText().toString();
+                final String password = etPassword .getText().toString();
+
+                Response.Listener<String> rl = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success){
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest rr = new RegisterRequest(email, password, rl);//response listener
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(rr);
+            }
+        });
     }
 }
