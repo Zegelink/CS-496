5d0c06e1ff8cc80a301062d87d2fe0b54d928369
Jake Smith <smitjaco>
2016-06-01 15:23:42 -0700

    Firebase Message Handler Class added

---
+++ app/src/main/java/zheng/studybuddy/FCMMessageHandler.java
@@ -0,0 +1,38 @@
+package zheng.studybuddy;
+
+/**
+ * Created by Jake on 6/1/2016.
+ */
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class FCMMessageHandler extends FirebaseMessagingService {
+    public static final int MESSAGE_NOTIFICATION_ID = 435345;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        String from = remoteMessage.getFrom();
+
+        Notification notification = remoteMessage.getNotification();
+        createNotification(notification);
+    }
+
+    // Creates notification based on title and body received
+    private void createNotification(Notification notification) {
+        Context context = getBaseContext();
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher).setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody());
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(MESSAGE_NOTIFICATION_ID, mBuilder.build());
+    }
+
+}
